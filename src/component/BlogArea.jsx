4e3395a784a7b66dcc77f689aea11e5215277835
raymond <dungraymond07@gmail.com>
2020-05-13 13:11:52 +0700

    finished base design

--- src/component/BlogArea.jsx
+++ src/component/BlogArea.jsx
@@ -4,9 +4,7 @@ class BlogArea extends Component{
     render(){
         return(
             <div className='blog-container'>
-                <div className='author-credit'>
-                    
-                </div>
+                <BlogAd />
                 <BlogPost />
                 <BlogPost />
                 <BlogPost />
@@ -15,15 +13,82 @@ class BlogArea extends Component{
     }
 }
 
-// class BlogAd extends Component {
-//     render(){
-//         return(
-//             <div className='ad-zone'>
+class BlogAd extends Component {
+    render(){
+        return(
+            <div className='author-credit'>
+                <div className='author-title'>
+                    <h2>About me</h2>
+                </div>
+                <div className='author-summary'>
+                    <p>
+                        C’est si bon
+                        De partir n’importe où,
+                        Bras-dessus, bras-dessous,
+                        En chantant des chansons.
+ 
+                        C’est si bon
+                        De se dire des mots doux,
+                        Des petits riens-du-tout,
+                        Mais qui en disent long.
+                    </p>
 
-//             </div>
-//         )
-//     }
-// }
+                </div>
+                <div className='author-title'>
+                    <h2>Facebook</h2>
+                </div>
+                <div className='author-summary'>
+                    <p>
+                        C’est si bon
+                        De partir n’importe où,
+                        Bras-dessus, bras-dessous,
+                        En chantant des chansons.
+ 
+                        C’est si bon
+                        De se dire des mots doux,
+                        Des petits riens-du-tout,
+                        Mais qui en disent long.
+                    </p>
+
+                </div>
+                <div className='author-title'>
+                    <h2>Instagram</h2>
+                </div>
+                <div className='author-summary'>
+                    <p>
+                        C’est si bon
+                        De partir n’importe où,
+                        Bras-dessus, bras-dessous,
+                        En chantant des chansons.
+ 
+                        C’est si bon
+                        De se dire des mots doux,
+                        Des petits riens-du-tout,
+                        Mais qui en disent long.
+                    </p>
+
+                </div>
+                <div className='author-title'>
+                    <h2>Follow Me</h2>
+                </div>
+                <div className='author-summary'>
+                    <p>
+                        C’est si bon
+                        De partir n’importe où,
+                        Bras-dessus, bras-dessous,
+                        En chantant des chansons.
+ 
+                        C’est si bon
+                        De se dire des mots doux,
+                        Des petits riens-du-tout,
+                        Mais qui en disent long.
+                    </p>
+
+                </div>
+            </div>
+        )
+    }
+}
 
 class BlogPost extends Component {
     render(){
@@ -35,7 +100,7 @@ class BlogPost extends Component {
 
                         </div>
                         <div className='post-title'>
-                            <h2>Lorem ipsum title here</h2>
+                            <h2>La vie en rose</h2>
                         </div>
                     </div>
                     <div className='post-info'>
@@ -44,7 +109,45 @@ class BlogPost extends Component {
                     </div>
                     <div className='post-text-content'>
                     <div className='text-box'>
-                        Lorem ipsum something here must be this long if not copy something until it's so long enough Lorem ipsum something here must be this long if not copy something until it's so long enough Lorem ipsum something here must be this long if not copy something until it's so long enough Lorem ipsum something here must be this long if not copy something until it's so long enough Lorem ipsum something here must be this long if not copy something until it's so long enough Lorem ipsum something here must be this long if not copy something until it's so long enough
+                    Des yeux qui font baisser les miens
+                    Un rire qui se perd sur sa bouche
+                    Voilà le portrait sans retouches
+                    De l'homme auquel j'appartiens
+                    Quand il me prend dans ses bras
+                    Il me parle tout bas
+                    Je vois la vie en rose
+                    Il me dit des mots d'amour
+                    Des mots de tous les jours
+                    Et ça me fait quelque chose
+                    Il est entré dans mon cœur
+                    Une part de bonheur
+                    Dont je connais la cause
+                    C'est lui pour moi, moi pour lui dans la vie
+                    Il me l'a dit, l'a juré pour la vie
+                    Et dès que je l'aperçois
+                    Alors je sens en moi
+                    Mon cœur qui bat
+                    Des nuits d'amour à plus finir
+                    Un grand bonheur qui prend sa place
+                    Des ennuis, des chagrins s'effacent
+                    Heureux, heureux à en mourir
+                    Quand il me prend dans ses bras
+                    Il me parle tout bas
+                    Je vois la vie en rose
+                    Il me dit des mots d'amour
+                    Des mots de tous les jours
+                    Et ça me fait quelque chose
+                    Il est entré dans mon cœur
+                    Une part de bonheur
+                    Dont je connais la cause
+                    C'est toi pour moi, moi pour toi dans la vie
+                    Il me l'a dit, l'a juré pour la vie
+                    Et dès que je t'aperçois
+                    Alors je sens dans moi
+                    Mon cœur qui bat
+                    La la, la la, la la
+                    La la, la la, ah la
+                    La la la la
                     </div>
                     
                     </div>
